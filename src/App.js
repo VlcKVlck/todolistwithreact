3b1735b886ccff31cab1cf9efc195e24123389ed
V I <vi285@nyu.edu>
2022-12-07 14:37:24 +0200

    with functions

--- src/App.js
+++ src/App.js
@@ -7,6 +7,32 @@ import Main from './components/Main';
 
 function App() {
     const appTitle = 'To Do';
+    let toDoList =[];
+
+    const addToDo =(taskName)=>{
+        toDoList.concat(toDoList, [{id: Date.now(), task: taskName, completed: false}]);
+    }
+
+    const removeTodo = (toDoToRemove) => {
+        toDoList = toDoList.filter(todo => todo.id !==toDoToRemove.id);
+
+    }
+
+    const toggleCompletedTask = (taskToToggleCompleted) =>{
+        toDoList.forEach(function (todo){
+            if (todo.id ===taskToToggleCompleted.id){
+                (todo.completed ===true) ? todo.completed=false : todo.completed =true;
+            }
+        })
+    }
+
+    const toggleCompletedAll = (checkBoxStatus) =>{
+        toDoList = toDoList.map( todo => ({ ...todo, completed: checkBoxStatus }));
+    }
+
+    const clearAllCompleted = ()=>{
+        toDoList = toDoList.filter(todo => !todo.completed)
+    }
 
 
   return (
