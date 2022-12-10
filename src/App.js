b5b95a7b9f16447016c00305312c3a59f53a8ac4
V I <vi285@nyu.edu>
2022-12-10 10:03:14 +0200

    fully worknig with inline editing

--- src/App.js
+++ src/App.js
@@ -13,34 +13,30 @@ function App() {
     const [ noneCompletedItemsCount, setNoneCompletedItemsCount ] = useState(0);
     console.log('entered app', "todolist;", toDoList)
 
-
     useEffect(() => {
-      const uncompleted = toDoList.filter( todo => todo.completed==false );
+      const uncompleted = toDoList.filter( todo => todo.completed===false );
       setNoneCompletedItemsCount(uncompleted.length);
       console.log(uncompleted, 'uncompleted')
   }, [toDoList])
 
     const addToDo =(taskName)=>{
         if (!taskName) return;
-        setToDoList (toDoList.concat( [{id: Date.now(), task: taskName, completed: false}]));
+        setToDoList (toDoList.concat( [{id: Date.now(), task: taskName, completed: false, editing: false}]));
         console.log('added task')
-        // console.log(toDoList, "after adding")
     }
 
     const removeTodo = (toDoToRemoveID) => {
-        // console.log(toDoToRemoveID)
-        setToDoList (toDoList.filter(todo => todo.id !=toDoToRemoveID));
+        setToDoList (toDoList.filter(todo => todo.id !==toDoToRemoveID));
     }
 
     const toggleCompletedTask = (taskId) =>{
         toDoList.forEach(todo=>{
-          if (todo.id ==taskId) {
+          if (todo.id ===taskId) {
             console.log('vicky2', toDoList);
             todo.completed = todo.completed !== true;
         }})
         setToDoList(toDoList.slice());
 }
-
 
     const toggleCompletedAll = (checkBoxStatus) =>{
         setToDoList (toDoList.map( todo => ({ ...todo, completed: checkBoxStatus})));
@@ -50,16 +46,16 @@ function App() {
         setToDoList (toDoList.filter(todo => !todo.completed));
     }
 
-    const changeTaskFromListOnDoubleClick =(e)=>{
-
+    function editToDo (todo, newTask){
+        todo.task = newTask;
+        setToDoList(toDoList.slice());
     }
-
 
   return (
   <section className="todoapp">
       <Header title ={appTitle} placeholder={placeHolder} addToDo = {addToDo}/>
       <Main toDoList = {toDoList} toggleCompletedAll={toggleCompletedAll} removeTodo={removeTodo} toggleCompletedTask={toggleCompletedTask}>
-        <List removeTodoFunc={removeTodo} toggleCompletedTask={toggleCompletedTask} changeTaskFromListOnDoubleClick={changeTaskFromListOnDoubleClick} toDoList = {toDoList} />
+        <List removeTodoFunc={removeTodo} editToDo={editToDo} toggleCompletedTask={toggleCompletedTask} toDoList = {toDoList} />
       </Main>
       <Footer clearAllCompleted={clearAllCompleted} openTasks={noneCompletedItemsCount} />
   </section>
--- src/components/List.js
+++ src/components/List.js
@@ -1,33 +1,31 @@
+import {useState} from "react";
 
 export default function List (props){
-    console.log('enteredlists')
+    const [editFlag, setEditFlag] = useState(false)
     function handleDeletion (e){
         props.removeTodoFunc (e.target.id);
     }
-
-    function HandleComplete (e){
-        props.toggleCompletedTask (e.target.id);
+    function handleDoubleClick (){
+        setEditFlag(!editFlag)
     }
 
-    function handleDoubleClick (e){
-        // e.target.className='editing';
-        console.log(e);
-    }
-//
+
     return (
         <ul className="todo-list" >
             {props.toDoList.map(todo => (
-                 <li key={todo.id}  className= {todo.completed? "completed" : ''}>
+                 <li key={todo.id}  className= {(todo.completed ? "completed" : '') + (editFlag ? 'editing' : '')}>
                     <div className="view" >
                         <input className="toggle"
-                               id={todo.id} type="checkbox" onChange={HandleComplete} checked={todo.completed ? true : false} />
-                        <label>{todo.task}</label>
+                               id={todo.id} type="checkbox" checked={todo.completed} />
+                        <label onDoubleClick={handleDoubleClick}>{todo.task}</label>
                         <button className="destroy" id={todo.id} onClick={handleDeletion} />
                     </div>
-                    <input className="edit"/>
+                    <input className="edit"
+                    value = {todo.task}
+                    onChange={(e) => props.editToDo (todo,e.target.value)}
+                    onKeyUp={(e)=>e.key==='Enter' && handleDoubleClick()}/>
                 </li>
             ))}
         </ul>
     )
-
 }
